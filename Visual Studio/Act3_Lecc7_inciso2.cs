8eca112effdda755cbd4491891714c026db1cf4d
25SCL01-SergioCalderon <scalderondb25@gmail.com>
2026-04-02 17:31:35 -0600

    Add files via upload

---
+++ Visual Studio/Act3_Lecc7_inciso2.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a1, a2, a3;
+
+        Console.WriteLine("Ingrese ángulo 1:");
+        a1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese ángulo 2:");
+        a2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese ángulo 3:");
+        a3 = int.Parse(Console.ReadLine());
+
+        if (a1 == a2 && a2 == a3)
+            Console.WriteLine("Triángulo Equilátero");
+        else if (a1 == a2 || a2 == a3 || a1 == a3)
+            Console.WriteLine("Triángulo Isósceles");
+        else
+            Console.WriteLine("Triángulo Escaleno");
+    }
+}
